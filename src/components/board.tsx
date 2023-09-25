1f784817ae14909ea20677510b0ed833bc101f51
Kidyoh <kidyoh789@gmail.com>
2023-09-25 12:14:54 +0300

    hotfix: synchronization implemented

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -7,9 +7,8 @@ import Confetti from 'react-dom-confetti';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Square } from './square.tsx';
 import { BoardProps } from '../types/player-info';
-import { DocumentData, DocumentReference, addDoc, collection, doc, setDoc, updateDoc} from '@firebase/firestore';
+import { doc, onSnapshot, updateDoc} from '@firebase/firestore';
 import { db } from '../firebase.tsx';
-
 
 const confettiConfig = {
   angle: 90,
@@ -24,40 +23,31 @@ const confettiConfig = {
   colors: ['#ff0000', '#00ff00', '#0000ff'],
 };
 
-
-
-
-
-function Board({ playerNames, updateLeaderboard, leaderboard, gameId: initialGameId }: BoardProps) {
+function Board({  updateLeaderboard, leaderboard, gameId: initialGameId }: BoardProps) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [showPopup, setShowPopup] = useState(false);
   const [winner, setWinner] = useState(null);
-  // const [winningSquares, setWinningSquares] = useState([]);
-
   const [gameId, setGameId] = useState<string | undefined | null>(initialGameId);
-  const [isCreating, setIsCreating] = useState<boolean>(false);
+  const playerNames = (location as { state?: { playerNames: { player1: string; player2: string } } }).state?.playerNames || { player1: '', player2: '' };
+
   useEffect(() => {
-    if (gameId || isCreating) return; // Don't create a game if a gameId is provided or if we're already creating a game.
+    if (!gameId) return;
 
-    setIsCreating(true);
+    const gameRef = doc(db, 'games', gameId);
+    const unsubscribe = onSnapshot(gameRef, (doc) => {
+      const data = doc.data();
+      if (!data) return;
 
-    //create game document and set current game
-    addDoc(collection(db, "games"), {
-      next_turn: "X",
-      board: squares
-    }).then((value: DocumentReference<DocumentData>) => {
-      console.log("Document written with ID: ", value.id);
-      setGameId(value.id);
-    }).catch((error) => {
-      console.log("Oops! error in creating game >> ", error);
-
-    }).finally(() => {
-      setIsCreating(false);
+      setSquares(data.squares);
+      setXIsNext(data.xIsNext);
+      setWinner(data.winner);
     });
-  }, []);
 
-
+    return () => {
+      unsubscribe();
+    };
+  }, [gameId]);
 
   function handleClick(i: number) {
     if (squares[i] || winner) {
@@ -74,17 +64,15 @@ function Board({ playerNames, updateLeaderboard, leaderboard, gameId: initialGam
       setWinner(newWinner);
       setShowPopup(true);
       updateLeaderboard(playerNames, '');
+      updateDoc(doc(db, 'games', gameId || ''), { squares: Array(9).fill(null), xIsNext: true, winner: null });
     } else if (newWinner) {
       setWinner(newWinner);
       setShowPopup(true);
       updateLeaderboard(playerNames, newWinner);
+      updateDoc(doc(db, 'games', gameId || ''), { squares: nextSquares, xIsNext: !xIsNext, winner: newWinner });
+    } else {
+      updateDoc(doc(db, 'games', gameId || ''), { squares: nextSquares, xIsNext: !xIsNext });
     }
-    if (gameId) {
-      const gameDocRef = doc(db, "games", gameId);
-      // updateDoc(gameDocRef, { squares: nextSquares });
-      setDoc(gameDocRef, {squares: nextSquares},{merge: true});
-    }
-    
   }
 
   function isBoardFull(squares: any[]) {
@@ -96,6 +84,7 @@ function Board({ playerNames, updateLeaderboard, leaderboard, gameId: initialGam
     setSquares(Array(9).fill(null));
     setWinner(null);
     setShowPopup(false);
+    updateDoc(doc(db, 'games', gameId || ''), { squares: Array(9).fill(null), xIsNext: true, winner: null });
   }
 
   if (!winner) {
@@ -110,77 +99,71 @@ function Board({ playerNames, updateLeaderboard, leaderboard, gameId: initialGam
   return (
     <>
       <div className="flex items-center justify-center">
-{!gameId &&(
-
-  <h5>Please Wait ....</h5>
-
-)}
-        {
-          gameId && (
-
-            <div className="flex justify-between mb-4">
-              <div className="w-1/4 mb-4 text-white">
+        {!gameId && (
+          <h5>Please Wait ....</h5>
+        )}
+        {gameId && (
+          <div className="flex justify-between mb-4">
+            <div className="w-1/4 mb-4 text-white">
               <PlayerCard
-        name={player1Name} 
-        profilePicture={player1Profile}
-        wins={player1Wins}
-        isCurrentPlayer={xIsNext} 
-      />
-              </div>
-              <div className="w-1/4 flex justify-center">
-                <div className="board">
-                  <div className="board-row flex">
-                    {squares.slice(0, 3).map((value, index) => (
-                      <Square key={index} value={value} onClick={() => handleClick(index)} isWinningSquare />
-                    ))}
-                  </div>
-                  <div className="board-row flex mt-1">
-                    {squares.slice(3, 6).map((value, index) => (
-                      <Square key={index + 3} value={value} onClick={() => handleClick(index + 3)} isWinningSquare />
-                    ))}
-                  </div>
-                  <div className="board-row flex mt-1">
-                    {squares.slice(6).map((value, index) => (
-                      <Square key={index + 6} value={value} onClick={() => handleClick(index + 6)} isWinningSquare />
-                    ))}
-                  </div>
+                name={player1Name} 
+                profilePicture={player1Profile}
+                wins={player1Wins}
+                isCurrentPlayer={xIsNext} 
+              />
+            </div>
+            <div className="w-1/4 flex justify-center">
+              <div className="board">
+                <div className="board-row flex">
+                  {squares.slice(0, 3).map((value, index) => (
+                    <Square key={index} value={value} onClick={() => handleClick(index)} isWinningSquare />
+                  ))}
+                </div>
+                <div className="board-row flex mt-1">
+                  {squares.slice(3, 6).map((value, index) => (
+                    <Square key={index + 3} value={value} onClick={() => handleClick(index + 3)} isWinningSquare />
+                  ))}
+                </div>
+                <div className="board-row flex mt-1">
+                  {squares.slice(6).map((value, index) => (
+                    <Square key={index + 6} value={value} onClick={() => handleClick(index + 6)} isWinningSquare />
+                  ))}
                 </div>
               </div>
-
-              <div className="w-1/4 mb-4 text-white">
-              <PlayerCard
-        name={player2Name}
-        profilePicture={player2Profile} 
-        wins={player2Wins}
-        isCurrentPlayer={!xIsNext}
-      />
-              </div>
             </div>
-          )
-        }
+            <div className="w-1/4 mb-4 text-white">
+              <PlayerCard
+                name={player2Name}
+                profilePicture={player2Profile} 
+                wins={player2Wins}
+                isCurrentPlayer={!xIsNext}
+              />
+            </div>
+          </div>
+        )}
       </div>
       <div>
-      <AnimatePresence>
-        {showPopup && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 12 }}
-            className="winner-popup fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center p-4 bg-white border border-gray-300 shadow-md"
-          >
-            <Confetti active={winner !== null} config={confettiConfig} />
-            <p className="text-xl mb-2">
-              {winner ? `Congratulations, ${playerName}! You won!` : "It's a draw!"}
-            </p>
-            <button
-              className="px-4 py-2 text-base bg-blue-500 text-white rounded-md transition duration-300 hover:bg-blue-700"
-              onClick={handleRestart}
+        <AnimatePresence>
+          {showPopup && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 12 }}
+              className="winner-popup fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center p-4 bg-white border border-gray-300 shadow-md"
             >
-              Play Again
-            </button>
-          </motion.div>
-        )}
-      </AnimatePresence>
+              <Confetti active={winner !== null} config={confettiConfig} />
+              <p className="text-xl mb-2">
+                {winner ? `Congratulations, ${playerName}! You won!` : "It's a draw!"}
+              </p>
+              <button
+                className="px-4 py-2 text-base bg-blue-500 text-white rounded-md transition duration-300 hover:bg-blue-700"
+                onClick={handleRestart}
+              >
+                Play Again
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </>
   );
--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -6,35 +6,43 @@ import { useNavigate } from 'react-router-dom';
 function HomePage() {
   const [gameId, setGameId] = useState('');
   const navigate = useNavigate();
+  const [player, setPlayer] = useState('');
 
   const createGame = async () => {
     try {
       const gameRef = await addDoc(collection(db, "games"), {
         state: 'waiting',
-        squares: [null, null, null, null, null, null, null, null, null], 
+        squares: [null, null, null, null, null, null, null, null, null],
+        playerOne: player,
+        playerTwo: '',
       });
-
+  
       const newGameId = gameRef.id;
       console.log(`Game created with ID: ${newGameId}`);
       setGameId(newGameId);
   
-      navigate('/game', { state: { gameId: newGameId } });
+      navigate('/game', { state: { gameId: newGameId, playerNames: { player1: player, player2: '' } } });
     } catch (error) {
       console.error('Error creating a new game:', error);
     }
   };
-
+  
   const joinGame = () => {
-    navigate('/game', { state: { gameId } });
+    navigate('/game', { state: { gameId, playerNames: { player1: '', player2: player } } });
   };
 
-  return (
-    <div>
-      <button onClick={createGame}>Create a Game</button>
-      <input value={gameId} onChange={e => setGameId(e.target.value)} placeholder="Enter game ID" />
-      <button onClick={joinGame}>Join a Game</button>
-    </div>
-  );
+
+return (
+  <div>
+    <label>
+      Enter your name:
+      <input value={player} onChange={e => setPlayer(e.target.value)} />
+    </label>
+    <button onClick={createGame}>Create a Game</button>
+    <input value={gameId} onChange={e => setGameId(e.target.value)} placeholder="Enter game ID" />
+    <button onClick={joinGame}>Join a Game</button>
+  </div>
+);
 }
 
 export default HomePage;
